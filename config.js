df473b0c9e1702d35ffcba1adeaa0ac87e402b0d
Sahil Kumar <sahilkumar142002@gmail.com>
2024-03-02 13:13:53 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET: "aGF2ZWFsaWtlY2x1YmxlZHBsYW5ldHVuaXRzdXJy",
+    EMAIL_USERNAME:"business.sahilkumar@gmail.com",
+    EMAIL_PASSWORD:'kagr ylwf xwzf ibbk',
+    ATLAS_URI:'mongodb+srv://secret-sahil:sahil123@cluster0.juyqf.mongodb.net/leaning?retryWrites=true&w=majority'
+}
