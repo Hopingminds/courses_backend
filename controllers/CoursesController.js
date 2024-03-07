bce408c010aaa38c1d995e87ddb737239c5d3990
Sahil Kumar <sahilkumar142002@gmail.com>
2024-03-07 18:10:04 +0530

    added search api

--- controllers/CoursesController.js
+++ controllers/CoursesController.js
@@ -20,18 +20,44 @@ export async function addcourse(req, res) {
 
 /** GET: http://localhost:8080/api/courses */
 export async function getCourses(req, res) {
+    let {category,subcategory,sort,price_min,price_max,search,populate} = req.query
 	try {
-        const courses = await CoursesModel.find({})
+		let query = {};
 
-        if (!courses) {
-            return res.status(404).json({ success: false, message: 'Courses not found' });
+		// Add category and subcategory to the query if provided
+		if (category) {
+			query.category = category;
+		}
+		if (subcategory) {
+			query.subcategory = subcategory;
+		}
+
+		// Add price range to the query if provided
+		if (price_min !== undefined && price_max !== undefined) {
+			query.base_price = { $gte: price_min, $lte: price_max };
+		} else if (price_min !== undefined) {
+			query.base_price = { $gte: price_min };
+		} else if (price_max !== undefined) {
+			query.base_price = { $lte: price_max };
+		}
+
+        if (search) {
+            query.title = { $regex: search, $options: 'i' };
         }
 
-        res.status(200).json({ success: true, courses });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, message: 'Internal server error' });
-    }
+		// Build the sort object based on the 'sort' parameter
+		let sortObj = {};
+		if (sort === 'price_asc') {
+			sortObj.base_price = 1;
+		} else if (sort === 'price_desc') {
+			sortObj.base_price = -1;
+		}
+		
+		const products = await CoursesModel.find(query).sort(sortObj).populate(populate)
+		res.status(200).json(products)
+	} catch (err) {
+		res.status(500).send('Internal Server Error')
+	}
 }
 
 /** GET: http://localhost:8080/api/course/:coursename */
