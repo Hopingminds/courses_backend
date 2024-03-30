18d3940c6414194abf189b120b0131b7589c4e6f
Sahil Kumar <sahilkumar142002@gmail.com>
2024-03-30 15:53:43 +0530

    fixed some apis and add comments

--- controllers/AwsController.js
+++ controllers/AwsController.js
@@ -2,37 +2,58 @@ import aws from 'aws-sdk'
 import multer from 'multer'
 import multerS3 from 'multer-s3'
 
+// upading AWS config
 aws.config.update({
     secretAccessKey: process.env.AWS_ACCESS_SECRET,
     accessKeyId: process.env.AWS_ACCESS_KEY,
     region: process.env.AWS_REGION,
-
 });
 
 const BUCKET = process.env.AWS_BUCKET
 const s3 = new aws.S3();
 
+// middleware
 export const upload = multer({
     storage: multerS3({
         s3: s3,
         acl: "public-read",
         bucket: BUCKET,
+        metadata: function (req, file, cb) {
+            console.log(file);
+            cb(null, {
+                fieldName: file.fieldname,
+                'Content-Type': file.mimetype,
+            });
+        },
         key: function (req, file, cb) {
-            cb(null, file.originalname)
+            cb(null, Date.now().toString()+file.originalname)
         }
     })
 })
 
+/** POST: http://localhost:8080/api/uploadfiletoaws
+    body:{
+        file: file.mp4
+    }
+**/
 export async function uploadFile(req, res) {
     return res.status(200).json({ success: true, message: 'Successfully Uploaded', url: req.file.location });
 }
 
+/** GET: http://localhost:8080/api/getfilesfromaws */
 export async function getfilesfromaws(req, res) {
     let r = await s3.listObjectsV2({ Bucket: BUCKET }).promise();
-    let data = r.Contents.map(item =>  `https://${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${item.Key}`);
+    let data = []
+    r.Contents.map(item => {
+        data.push({
+            key: item.Key,
+            url:  `https://${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${item.Key}`
+        })
+    })
     return res.status(200).json({success: true, data})
 }
 
+/** GET: http://localhost:8080/api/getfilefromaws/:filename */
 export async function getfilefromaws(req, res) {
     const filename = req.params.filename
     
@@ -47,6 +68,7 @@ export async function getfilefromaws(req, res) {
     }
 }
 
+/** DELETE: http://localhost:8080/api/deletefilefromaws/:filename */
 export async function deleteFileFromAWS(req, res) {
     const filename = req.params.filename
     
