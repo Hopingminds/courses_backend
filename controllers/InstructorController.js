2535a5b8805a9d36ef552d2544fd3548f749a8ba
Sahil Kumar <sahilkumar142002@gmail.com>
2024-04-06 12:42:15 +0530

    added instructor apis

--- controllers/InstructorController.js
+++ controllers/InstructorController.js
@@ -19,9 +19,10 @@ export async function verifyInstructor(req, res, next) {
 
 /** POST: http://localhost:8080/api/register 
 * @param : {
-    "username" : "example123",
-    "password" : "admin123",
+	"name": "Sahil Kumar"
+	"password" : "admin123",
     "email": "example@gmail.com",
+    "phone" : 9814740275,
 	"college": "IKGPTU", 
 	"stream": "B.tech ECE" , 
 	"yearofpass": 2024
@@ -29,23 +30,7 @@ export async function verifyInstructor(req, res, next) {
 */
 export async function register(req, res) {
 	try {
-		const { username, password, profile, email, college, stream , yearofpass } = req.body
-
-		// check the existing instructor
-		// const existUsername = new Promise((resolve, reject) => {
-		// 	InstructorModel.findOne({ username })
-		// 		.exec()
-		// 		.then((instructor) => {
-		// 			if (instructor) {
-		// 				reject({ error: 'Please use a unique username' })
-		// 			} else {
-		// 				resolve()
-		// 			}
-		// 		})
-		// 		.catch((err) => {
-		// 			reject(new Error(err))
-		// 		})
-		// })
+		const {password, name, profile, email, experience, social_links, phone, bio } = req.body
 
 		// check for existing email
 		const existEmail = new Promise((resolve, reject) => {
@@ -53,7 +38,7 @@ export async function register(req, res) {
 				.exec()
 				.then((email) => {
 					if (email) {
-						reject({ error: 'Please use a unique email' })
+						reject({ error: 'Email already exsists!' })
 					} else {
 						resolve()
 					}
@@ -70,21 +55,39 @@ export async function register(req, res) {
 						.hash(password, 10)
 						.then((hashedPassword) => {
 							const instructor = new InstructorModel({
-								// username,
+								// instructorname,
 								password: hashedPassword,
 								profile: profile || '',
-								email,
-								college, 
-								stream , 
-								yearofpass
+								name, email, experience, social_links, phone, bio
 							})
 
 							// return save result as a response
 							instructor.save()
-								.then((result) =>
-									res.status(201).send({
-										msg: 'Instructor Register Successfully',
+								.then((instructor) =>{
+									const token = jwt.sign(
+										{
+											instructorID: instructor._id,
+											email: instructor.email,
+											role: instructor.role,
+										},
+										process.env.JWT_SECRET,
+										{ expiresIn: '24h' }
+									)
+									
+									InstructorModel.updateOne({ email:instructor.email }, { token })
+									.exec()
+									.then(()=>{
+										return res.status(201).send({
+											msg: 'Instructor Register Successfully',
+											email: instructor.email,
+											role: instructor.role,
+											token,
+										})
 									})
+									.catch((error)=>{
+										return res.status(200).json({ success: false, message: 'Internal Server Error - Error Saving Token', error});
+									})
+								}
 								)
 								.catch((error) =>
 									res.status(500).send({ error })
@@ -101,13 +104,13 @@ export async function register(req, res) {
 				return res.status(500).send({ error })
 			})
 	} catch (error) {
-		return res.status(500).send(error)
+		return res.status(500).json({ success: false, message: 'Internal server error' });
 	}
 }
 
 /** POST: http://localhost:8080/api/login 
 * @param : {
-    "email" : "example123@gmail.com",
+    "instructorname" : "example123",
     "password" : "admin123",
 }
 */
@@ -122,21 +125,31 @@ export async function login(req, res) {
 						if (!passwordCheck)
 							return res
 								.status(400)
-								.send({ error: "Don't password" })
+								.send({ error: "Password does not match" })
 
 						// create jwt token
 						const token = jwt.sign(
 							{
 								instructorID: instructor._id,
 								email: instructor.email,
+								role: instructor.role,
 							},
 							process.env.JWT_SECRET,
 							{ expiresIn: '24h' }
 						)
-						return res.status(200).send({
-							msg: 'Login Successful',
-							email: instructor.email,
-							token,
+						
+						InstructorModel.updateOne({ email }, { token })
+						.exec()
+						.then(()=>{
+							return res.status(200).send({
+								msg: 'Login Successful',
+								email: instructor.email,
+								role: instructor.role,
+								token,
+							})
+						})
+						.catch((error)=>{
+							return res.status(200).json({ success: false, message: 'Internal Server Error - Error Saving Token', error});
 						})
 					})
 					.catch((error) => {
@@ -146,7 +159,7 @@ export async function login(req, res) {
 					})
 			})
 			.catch((error) => {
-				return res.status(404).send({ error: 'email not Found' })
+				return res.status(404).send({ error: 'Email not Found' })
 			})
 	} catch (error) {
 		return res.status(500).send(error)
@@ -169,7 +182,7 @@ export async function getInstructor(req, res) {
 						reject({ error: "Couldn't Find the Instructor" })
 					} else {
 						// Remove sensitive information (e.g., password) before resolving the promise
-						const { password, ...rest } = instructor.toObject()
+						const { password, token, ...rest } = instructor.toObject()
 						resolve(rest)
 					}
 				})
@@ -179,8 +192,8 @@ export async function getInstructor(req, res) {
 		})
 
 		Promise.all([checkInstructor])
-			.then((userDetails) => {
-				return res.status(200).send(userDetails)
+			.then((instructorDetails) => {
+				return res.status(200).send({instructorDetails:instructorDetails[0]})
 			})
 			.catch((error) => {
 				return res.status(500).send({ error: error.message })
@@ -190,16 +203,15 @@ export async function getInstructor(req, res) {
 	}
 }
 
-/** PUT: http://localhost:8080/api/updateuser 
+/** PUT: http://localhost:8080/api/updateinstructor 
  * @param: {
     "header" : "<token>"
 }
 body: {
-    "username" : "",
+    "instructorname" : "",
     
     "email": "",
-    "firstName": "",
-    "lastName": "",
+    "name": "",
     "profile": "",
     "college": "",
     "position": "",
@@ -237,15 +249,6 @@ export async function updateInstructor(req, res) {
 	}
 }
 
-// successfully redirect instructor when OTP is valid
-/** GET: http://localhost:8080/api/createResetSession */
-export async function createResetSession(req, res) {
-	if(req.app.locals.resetSession){
-        return res.status(201).send({ flag : req.app.locals.resetSession})
-    }
-    return res.status(440).send({error : "Session expired!"})
-}
-
 // update the password when we have valid session
 /** PUT: http://localhost:8080/api/resetPassword 
  * body:{
@@ -258,15 +261,15 @@ export async function resetPassword(req,res){
         
         if(!req.app.locals.resetSession) return res.status(440).send({error : "Session expired!"});
 
-        const { username, password } = req.body;
+        const { email, password } = req.body;
 
         try {
             
-            InstructorModel.findOne({ username})
+            InstructorModel.findOne({ email})
                 .then(instructor => {
                     bcrypt.hash(password, 10)
                         .then(hashedPassword => {
-                            InstructorModel.updateOne({ username : instructor.username },
+                            InstructorModel.updateOne({ email : instructor.email },
                             { password: hashedPassword})
                             .exec()
                             .then(()=>{
@@ -284,7 +287,7 @@ export async function resetPassword(req,res){
                         })
                 })
                 .catch(error => {
-                    return res.status(404).send({ error : "Username not Found"});
+                    return res.status(404).send({ error : "Email not Found"});
                 })
 
         } catch (error) {
--- model/Instructor.model.js
+++ model/Instructor.model.js
@@ -1,22 +1,21 @@
 import mongoose from 'mongoose'
 
-export const CategoriesSchema = new mongoose.Schema({
-	firstName: { type: String },
-	lastName: { type: String },
-	about: { type: String },
+export const InstructorSchema = new mongoose.Schema({
+	name: { type: String },
+	bio: { type: String },
 	profile: { type: String },
     email: {type: String},
+    phone: {type: Number},
     password: {type: String},
-	total_students: { type: String, default: 0 },
-	total_lessons: { type: Number, default: 0 },
 	experience: { type: String },
 	social_links: [
 		{
 			website_name: { type: String },
 			profile_url: { type: String },
 		},
-	]
+	],
+	token: {type: String}
 })
 
-export default mongoose.model.Categories ||
-	mongoose.model('Categories', CategoriesSchema)
+export default mongoose.model.Instructors ||
+	mongoose.model('Instructor', InstructorSchema)
