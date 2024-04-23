1a9aa6a3f2148f6de93ce14e8622a9824385c540
Sahil Kumar <sahilkumar142002@gmail.com>
2024-04-23 11:22:50 +0530

    updaed testSubmittedUsersList

--- controllers/QnaController.js
+++ controllers/QnaController.js
@@ -187,31 +187,25 @@ function hidePhone(phone) {
 export async function testSubmittedUsersList(req, res) {
     try {
         const completedReports = await UsertestreportModel.find({}).populate('user', 'name phone').exec();
-        
-        // Create an object to store user data temporarily
+
         const userData = {};
 
-        // Iterate through completedReports to gather user data
         completedReports.forEach(report => {
             const userId = report.user._id.toString();
 
-            // Initialize user object if not already initialized
             if (!userData[userId]) {
                 userData[userId] = {
                     name: report.user.name,
                     phone: hidePhone(report.user.phone),
-                    allModulesCompleted: true // Assume all modules are completed until proven otherwise
+                    allModulesCompleted: true
                 };
             }
 
-            // Check if the report is completed for the user
             if (!report.isModuleCompleted) {
-                // If any report is not completed, mark allModulesCompleted as false
                 userData[userId].allModulesCompleted = false;
             }
         });
-
-        // Filter out users whose allModulesCompleted is true
+        
         const uniqueUsers = Object.values(userData).filter(user => user.allModulesCompleted);
 
         return res.status(200).send({ success: true, data: uniqueUsers });
