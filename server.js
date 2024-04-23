18789a6a22b8a2b92333f6c34e6cce9eb380329d
Sahil Kumar <sahilkumar142002@gmail.com>
2024-04-23 18:37:21 +0530

    fixed cros error

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json())
-const allowedOrigins=["http://localhost:3000","http://hopingminds.in"]
+const allowedOrigins=["http://localhost:3000","http://hopingminds.in", "https://courses-admin-nine.vercel.app/login"]
 app.use(cors({
     origin: allowedOrigins,
     methods: "GET,POST,PUT,DELETE",
