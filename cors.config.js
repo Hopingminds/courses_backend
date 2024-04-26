2dc75f54e21fe06463bd0067f3aa8bfd3c232051
Sahil Kumar <sahilkumar142002@gmail.com>
2024-04-26 12:13:57 +0530

    cors updated

--- cors.config.js
+++ cors.config.js
@@ -1,8 +1,7 @@
 const CorsConfig = [
     "http://localhost:3000",
-    "http://hopingminds.in",
+    "https://hopingminds.in",
     "https://courses-admin-nine.vercel.app",
-    "https://precious-marigold-5dd5d3.netlify.app"
 ]
 
 export default CorsConfig
