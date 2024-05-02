432b77a471b9044b06faaef7a844ea18dddd4d7b
Sahil Kumar <sahilkumar142002@gmail.com>
2024-05-02 10:27:40 +0530

    updated JobopeningsSchema

--- model/Jobopenings.model.js
+++ model/Jobopenings.model.js
@@ -1,17 +1,66 @@
 import mongoose from 'mongoose'
 
 export const JobopeningsSchema = new mongoose.Schema({
-	position: {
+    position: {
         type: String,
     },
+    employment_type: {
+        type: String
+    },
+    key_skills: {
+        type: String
+    },
     company: {
+        type: String,
+    },
+    role_category: {
+        type: String,
+    },
+    work_mode: {
+        type: String,
+    },
+    location: {
+        type: String,
+    },
+    work_experience: {
+        from: { type: String },
+        to: { type: String },
+    },
+    annual_salary_range: {
+        from: { type: String },
+        to: { type: String },
+    },
+    company_industry: {
+        type: String,
+    },
+    educational_qualification: {
+        type: String,
+    },
+    specialization: {
+        type: String,
+    },
+    interview_mode: {
+        type: String,
+    },
+    job_description: {
+        type: String,
+    },
+    about_company: {
+
+    },
+    company_website_link: {
+        type: String,
+    },
+    company_address: {
         type: String,
     },
     logoUrl: {
         type: String,
     },
-    location: {
+    publishStatus: {
         type: String,
+        default: "active",
+        enum: ['active', 'closed']
     },
     publishDate: {
         type: Date,
