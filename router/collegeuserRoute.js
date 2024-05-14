c189c15f3a015ddec51d648a7936025de2bba0a7
Sahil Kumar <sahilkumar142002@gmail.com>
2024-05-14 16:40:08 +0530

    added AdminAuth to registercollegeUser and get | collegeUsers

--- router/collegeuserRoute.js
+++ router/collegeuserRoute.js
@@ -3,16 +3,17 @@ const router = Router()
 
 import * as collegeUserController from '../controllers/CollegeUserController.js'
 import CollegeUserAuth from '../middleware/collegeUserauth.js'
+import AdminAuth from '../middleware/adminauth.js'
 
 // POST ROUTES
-router.route('/registercollegeUser').post(collegeUserController.register)
+router.route('/registercollegeUser').post(AdminAuth, collegeUserController.register)
 router.route('/authenticatecollegeUser').post(CollegeUserAuth,(req,res)=>res.end())
 router.route('/loginCollegeUserWithEmail').post(collegeUserController.verifyCollegeUser,collegeUserController.loginWithEmail)
 router.route('/loginCollegeUserWithMobile').post(collegeUserController.verifyCollegeUser,collegeUserController.loginWithMobile)
 
 // GET ROUTES
 router.route('/collegeUser').get(collegeUserController.verifyCollegeUser, collegeUserController.getCollegeUser)
-router.route('/collegeUsers').get(CollegeUserAuth, collegeUserController.getallCollegeUsers)
+router.route('/collegeUsers').get(AdminAuth, collegeUserController.getallCollegeUsers)
 
 // PUT ROUTES
 router.route('/updatecollegeUser').put(CollegeUserAuth, collegeUserController.updateCollegeUser)
