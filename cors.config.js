ab7bbaa28db4ec5f3f72bf11af61facb85a90322
Sahil Kumar <sahilkumar142002@gmail.com>
2024-06-07 17:41:18 +0530

    updated cors

--- cors.config.js
+++ cors.config.js
@@ -2,6 +2,7 @@ const CorsConfig = [
     "http://localhost:3000",
     "https://hopingminds.in",
     "https://courses-admin-fawn.vercel.app",
+    "https://hmcodelab.com"
 ]
 
 export default CorsConfig
