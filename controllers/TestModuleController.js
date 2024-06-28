0ccdf7632cee5db10bc57e4a8f01e3d41e869fa0
Sahil Kumar <sahilkumar142002@gmail.com>
2024-06-28 12:07:23 +0530

    isSuspended True

--- controllers/TestModuleController.js
+++ controllers/TestModuleController.js
@@ -92,7 +92,8 @@ export async function getAllModules(req, res) {
 		userProgress.forEach(module => {
 			const progress = calculateProgress(module);
 			const isModuleCompleted = module.isModuleCompleted;
-			progressMap.set(module.module.toString(), { progress, isModuleCompleted });
+			const isSuspended = module.isSuspended;
+			progressMap.set(module.module.toString(), { progress, isModuleCompleted, isSuspended });
 		});
 
 		if (userProgress.length === 0) {
@@ -108,12 +109,13 @@ export async function getAllModules(req, res) {
 
 		const data = modules.map(module => {
 			const { questions, ...rest } = module;
-			const progressObj = progressMap.get(module._id.toString()) || { progress: 0, isModuleCompleted: false };
-			const { progress, isModuleCompleted } = progressObj;
-			return { ...rest, isModuleCompleted, progress };
+			const progressObj = progressMap.get(module._id.toString()) || { progress: 0, isModuleCompleted: false, isSuspended: false };
+            console.log(progressObj);
+            const { progress, isModuleCompleted, isSuspended } = progressObj;
+			return { ...rest, isModuleCompleted, isSuspended, progress };
 		});
 
-		return res.status(200).send({ success: true, isTestCompleted, isSuspended:userProgress.isSuspended, data });
+		return res.status(200).send({ success: true, isTestCompleted, data });
 	} catch (error) {
 		return res.status(500).send({ error: 'Internal Server Error', error });
 	}
