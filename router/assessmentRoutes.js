158460bfa478a213233eac2f501b894f9cca6032
AnshulKaryal <karyalanshul@gmail.com>
2024-08-27 17:52:25 +0530

    "Added AdminAuth middleware to several routes in assessmentRoutes.js"

--- router/assessmentRoutes.js
+++ router/assessmentRoutes.js
@@ -11,22 +11,22 @@ import Auth from '../middleware/auth.js';
 router.route('/createcourseassessment').post(AdminAuth, upload.single('questions'), AssessmentController.createAssessment);
 router.route('/submitassessment').post(Auth, AssessmentController.submitAssessment);
 router.route('/resetassessment').post(Auth, AssessmentController.requestForReassessment);
-router.route('/createmoduleassessment').post( ModuleAssessmentController.createModuleAssessment);
-router.route('/addquestionstoassessmentmodule').post( ModuleAssessmentController.addQuestionsToModuleAssessment);
+router.route('/createmoduleassessment').post(AdminAuth, ModuleAssessmentController.createModuleAssessment);
+router.route('/addquestionstoassessmentmodule').post(AdminAuth, ModuleAssessmentController.addQuestionsToModuleAssessment);
 
 // GET ROUTES
 router.route('/courseassessments/:coursename').get(Auth, AssessmentController.getCourseAllAssessment);
 router.route('/getassessment').get(Auth, AssessmentController.getAssesment);
-router.route('/getmoduleassessment/:moduleAssessmentid').get( ModuleAssessmentController.getModuleAssessment);
+router.route('/getmoduleassessment/:moduleAssessmentid').get(AdminAuth, ModuleAssessmentController.getModuleAssessment);
 
 //PUT ROUTES
 router.route('/submitassessmentanswer').put(Auth, AssessmentController.submitAnswerForAssessment);
 router.route('/submitassessment').put(Auth, AssessmentController.finishAssessment);
 router.route('/updateassessment').put(AdminAuth, AssessmentController.updateAssessment);
-router.route('/updatemoduleassessment/:moduleAssessmentid').put( ModuleAssessmentController.editModuleAssessment);
+router.route('/updatemoduleassessment/:moduleAssessmentid').put(AdminAuth, ModuleAssessmentController.editModuleAssessment);
 
 //DELETE ROUTES
 router.route('/deleteassessment').delete(AdminAuth, AssessmentController.deleteAssessment)
-router.route('/deletemodulefromassessment').delete( ModuleAssessmentController.deleteModuleFromAssessment)
+router.route('/deletemodulefromassessment').delete(AdminAuth, ModuleAssessmentController.deleteModuleFromAssessment)
 
 export default router;
