75e55c3d9663d8f740f7cb8260505dd8594f3e38
AnshulKaryal <karyalanshul@gmail.com>
2024-10-05 15:42:35 +0530

    "Updated express.json and express.urlencoded middleware with limit and extended options"

--- server.js
+++ server.js
@@ -47,7 +47,8 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.json())
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.use(cors({
     origin: CorsConfig,
