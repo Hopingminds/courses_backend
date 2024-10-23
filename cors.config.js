0953d2be4296696438433bf87d5b33e2b556418b
Anshul Karyal <113912205+AnshulKaryal@users.noreply.github.com>
2024-10-23 11:39:58 +0530

    Update cors.config.js

--- cors.config.js
+++ cors.config.js
@@ -3,6 +3,7 @@ const CorsConfig = [
     "http://localhost:3001",
     "https://hopingminds.com",
     "https://courses-admin-gules.vercel.app",
+    "https://courses-admin-wine.vercel.app",
     "https://hmcodelab.com",
     "https://www.hopingminds.com",
     "https://teacher-panel-nu.vercel.app",
