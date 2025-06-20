d397d9d37af92b3b1da0f360569a8dd82ddc4ebb
Sahil Kumar <sahilkumar142002@gmail.com>
2025-06-20 15:50:43 +0530

    refactor: remove unnecessary await from registerMail calls in user registration

--- controllers/appController.js
+++ controllers/appController.js
@@ -64,7 +64,7 @@ export async function register(req, res) {
 
 		// Send registration email
 		let mailSent = false;
-		await registerMail({
+		registerMail({
             body: {
                 username: name,
                 userEmail: email,
